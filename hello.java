097e1811a498c6746b36c77753366de73d651b63
Mandy <mandy.ou@esmartwave.com>
2020-01-01 21:54:48 +0800

    text

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
     public static void main(String [] args)
     {
+		System.out.println("hello text");
         System.out.println("Hello world");
 		System.out.println("Hello Finhao Fang");
     }   
